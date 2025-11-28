01ee53d012fa9b968f038889e9783fbdf1d50a56
K4uaBorges <Borgeskaua296@gmail.com>
2025-11-28 12:30:07 +0000

    Refactor play function for turn handling
    Just some improvements in function play

--- src/main/kotlin/game/table/Search.kt
+++ src/main/kotlin/game/table/Search.kt
@@ -39,7 +39,7 @@ import java.util.HashMap
 
 class Search(val table: Table) {
 
-        // Search in Diagonal invert (UP RIGHT to DOWN LEFT) and not invert (UP LEFT to DOWN RIGHT)
+    // Search in Diagonal invert (UP RIGHT to DOWN LEFT) and not invert (UP LEFT to DOWN RIGHT)
     fun searchInDiag(p: Piece, invert: Boolean): HashMap<Position, Directions> {
         val targetColor = p.color
         val start = p.position
@@ -50,41 +50,38 @@ class Search(val table: Table) {
             Pair(UP_RIGHT, DOWN_LEFT)
         else
             Pair(UP_LEFT, DOWN_RIGHT)
+run {
+    val acc = mutableListOf<Position>()
+    var pos1 = start.plus(dir1)
+    var pos2 = start.plus(dir2)
+    var closed = false
 
-            // Direction 1
-        run {
-            val acc = mutableListOf<Position>()
-            var pos = start.plus(dir1)
-            var closed = false
+    while (table.inBound(pos1) && table.getPiece(pos1) != null) {
+        if(table.getPiece(start.plus(dir1))!!.color == targetColor) { break }
+        val pieceCur = table.getPiece(pos1)!!
+        acc.add(pos1)
+        if (pieceCur.color == targetColor) { closed = true; break }
+        pos1 = pos1.plus(dir1)
+    }
 
-            while (table.inBound(pos) && table.getPiece(pos) != null) {
-                if(table.getPiece(start.plus(dir1))!!.color == targetColor) { break }
-                val pieceCur = table.getPiece(pos)!!
-                acc.add(pos)
-                if (pieceCur.color == targetColor) { closed = true; break }
-                pos = pos.plus(dir1)
-            }
-            if (closed) {
-                acc.forEach { result[it] = dir1 }
-            }
-        }
+    if (closed) {
+        acc.forEach { result[it] = dir1 }
+    }
 
-            // Direction 2
-        run {
-            val acc = mutableListOf<Position>()
-            var pos = start.plus(dir2)
-            var closed = false
-            while (table.inBound(pos) && table.getPiece(pos) != null) {
-                if(table.getPiece(start.plus(dir2))!!.color == targetColor) { break }
-                val pieceCur = table.getPiece(pos)!!
-                acc.add(pos)
-                if (pieceCur.color == targetColor) { closed = true; break }
-                pos = pos.plus(dir2)
-            }
-            if (closed) {
-                acc.forEach { result[it] = dir2 }
-            }
-        }
+    closed = false
+
+    while (table.inBound(pos2) && table.getPiece(pos2) != null) {
+        if(table.getPiece(start.plus(dir2))!!.color == targetColor) { break }
+        val pieceCur = table.getPiece(pos2)!!
+        acc.add(pos2)
+        if (pieceCur.color == targetColor) { closed = true; break }
+        pos2 = pos2.plus(dir2)
+    }
+
+    if (closed) {
+        acc.forEach { result[it] = dir2 }
+    }
+}
 
         return result
     }
@@ -131,7 +128,7 @@ class Search(val table: Table) {
         return result
     }
 
-        // Search in column if it doesn't have some any piece in down search to up, and same in reverse
+    // Search in column if it doesn't have some any piece in down search to up, and same in reverse
     fun searchInCol(p: Piece): HashMap<Position, Directions> {
         val targetColor = p.color
         val start = p.position
